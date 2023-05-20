e931c83a0ac092a8610104fb4f9a1dae13874883
Emerson Camilo <emersoncamilo@hotmail.com>
2023-05-20 14:58:07 +0100

    proxy

--- crud-spring/src/main/java/com/spring/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/spring/crudspring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.spring.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.spring.crudspring.model.Course;
+import com.spring.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,25 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c1 = new Course();
+			c1.setName("Angular");
+			c1.setCategory("front-end");
+
+			Course c2 = new Course();
+			c2.setName("Spring");
+			c2.setCategory("back-end");
+
+
+			
+			courseRepository.save(c1);
+
+			courseRepository.save(c2);
+		};
+
+	}
 }

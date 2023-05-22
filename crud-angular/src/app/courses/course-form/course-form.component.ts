317870b11b3f21c23a15585835026efff1a6999b
Emerson Camilo <emersoncamilo@hotmail.com>
2023-05-22 20:45:45 +0100

    Refactor to typed forms

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,6 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from '../services/courses.service';
@@ -7,32 +8,47 @@ import { CoursesService } from '../services/courses.service';
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent {
 
-  form: FormGroup;
+  form  = this.formBuilder.group({
+    name: [''],
+    category: [''],
+  });
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar){
-    this.form=this.formBuilder.group({
-      name:[null],
-      category:[null]
-    });
-  };
+    private snackBar: MatSnackBar,
+    private location: Location
+  ) {
+  }
 
-  onSubmit(){
-    this.service.save(this.form.value)
-      .subscribe(result => console.log(result), error => this.onError());
+  ngOnInit(): void {
+    this.form.value.name = 'ola';
+
+  }
+
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(
+      (result) => this.onSuccess(),
+      (error) => this.onError()
+    );
   }
 
   onCancel() {
-    console.log("onCancel");
-
+    this.location.back();
   }
 
-  private onError(){
-    this.snackBar.open("Error to save course", '',{duration: 2000} );
+  private onSuccess() {
+    this.snackBar.open('Course save!', '', { duration: 4000 });
+    this.onCancel();
+  }
+
+  private onError() {
+    this.snackBar.open('Error while try to save course', '', {
+      duration: 2000,
+    });
   }
 }
--- crud-spring/src/main/java/com/spring/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/spring/crudspring/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.spring.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.crudspring.model.Course;
@@ -23,5 +28,18 @@ public class CourseController {
     public List<Course> list() {
         return courseRepository.findAll();
     }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+        return courseRepository.save(course);
+        
+    }
+
+    // other way to create a response
+    // @PostMapping
+    // public ResponseEntity<Course> create(@RequestBody Course course){
+    //     return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+    // }
 
 }

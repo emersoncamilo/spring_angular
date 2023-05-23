ee11489045666f6ceeb23031ba4d24520c157bf0
Emerson Camilo <emersoncamilo@hotmail.com>
2023-05-23 16:01:37 +0100

    back end findById

--- crud-spring/src/main/java/com/spring/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/spring/crudspring/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +42,12 @@ public class CourseController {
     // public ResponseEntity<Course> create(@RequestBody Course course){
     //     return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     // }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+        return courseRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
+    }
 
 }
